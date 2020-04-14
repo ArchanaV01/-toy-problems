72607c70442e8f8dbdcafd02040191059fdb3923
ArchanaV01 <55693584+ArchanaV01@users.noreply.github.com>
2020-04-14 16:58:29 +0530

    Create LRUTest.py

---
+++ LRUTest.py
@@ -0,0 +1,19 @@
+import LRU
+class LRUTest:
+    def __main__():
+        lru_obj = LRU(3)
+        #tc1
+        assert lru_obj.get('a')==-1
+        #tc2
+        lru_obj.put('a',1)
+        lru_obj.put('b',2)
+        lru_obj.put('c',3)
+        assert lru_obj.get('a')==1, 'Wrong Value'
+        #tc3
+        assert lru_obj.get_cache()== {'a':1, 'b':2, 'c':3}, 'Cache is not updated properly'
+        #tc4
+        lru_obj.put('d',4)
+        #tc5
+        assert len(lru_obj.get_cache())==3, 'capacity crossed'
+        #tc6
+        assert lru_obj.get('a')==-1, 'Least Recently Used is not being removed when capacity is crossed'
